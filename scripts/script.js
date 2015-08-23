d8a39ed6d83558f1a6bd5e0c16cab1b269f9d1a9
Kharda <ahfa05@yahoo.co.id>
2015-08-23 07:17:28 +0700

    Add gMaps to show search result

--- scripts/script.js
+++ scripts/script.js
@@ -6,28 +6,53 @@ function myFunction() {
 	});
     alert("I am an alert box!");
 }
+	
+	function initialize()
+{
+var locations = [
+      ['Pasar Jaya Sawah Besar', -6.1540989, 106.8233203, 1],
+	  ['Pasar Jaya Senen', -6.237207, 106.8257511, 2],
+	  ['HERO Fatmawati', -6.301283, 106.7949294, 3]
+    ];
 
-	function initialize() {
-        var myLatlng = new google.maps.LatLng(40.779502, -73.967857);
-        var myOptions = {
-            zoom: 12,
-            center: myLatlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+      zoom: 11,
+      center: new google.maps.LatLng(-6.2297465, 106.829518),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
         }
-        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-        TestMarker();
+      })(marker, i));
     }
+}
 
-    // Function for adding a marker to the page.
-    function addMarker(location) {
-        marker = new google.maps.Marker({
-            position: location,
-            map: map
-        });
-    }
-
-    // Testing the addMarker function
-    function TestMarker() {
-           CentralPark = new google.maps.LatLng(37.7699298, -122.4469157);
-           addMarker(CentralPark);
-    }
+function loadScript()
+{
+/** 
+ var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=&sensor=false&callback=initialize";
+  document.body.appendChild(script);
+  */
+  
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = src="http://maps.google.com/maps/api/js?sensor=false&callback=initialize";
+  document.body.appendChild(script);
+  
+  
+}
